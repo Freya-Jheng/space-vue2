1560c1f19a14c20ca42662f036ab3ac192853717
Freya Jheng <freyajheng@gmail.com>
2022-05-08 15:27:26 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/space-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,3 +12,8 @@ export default defineConfig({
     }
   }
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/space-vue/'
+//     : '/'
+// }
